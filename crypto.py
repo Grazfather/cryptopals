4cb43a8f12898d599acf49c83948373829361854
Grazfather <Grazfather@gmail.com>
2015-07-13 20:47:23 -0700

    Set1: Complete challenge3, add 'score_english'

--- crypto.py
+++ crypto.py
@@ -1,8 +1,10 @@
 import base64
+from collections import Counter
+from itertools import izip
 
 
 def xorstring(s1, s2):
-   return "".join([chr(ord(a) ^ ord(b)) for a, b in zip(s1, s2)])
+    return "".join([chr(ord(a) ^ ord(b)) for a, b in izip(s1, s2)])
 
 
 def hex_to_str(s):
@@ -27,3 +29,54 @@ def base64_to_str(s):
 
 def base64_to_hex(s):
     return str_to_hex(base64_to_str(s))
+
+ENG_FREQ_MAP = {
+    " ": 18.28846265,
+    "E": 10.26665037,
+    "T": 7.51699827,
+    "A": 6.53216702,
+    "O": 6.15957725,
+    "N": 5.71201113,
+    "I": 5.66844326,
+    "S": 5.31700534,
+    "R": 4.98790855,
+    "H": 4.97856396,
+    "L": 3.31754796,
+    "D": 3.28292310,
+    "U": 2.27579536,
+    "C": 2.23367596,
+    "M": 2.02656783,
+    "F": 1.98306716,
+    "W": 1.70389377,
+    "G": 1.62490441,
+    "P": 1.50432428,
+    "Y": 1.42766662,
+    "B": 1.25888074,
+    "V": 0.79611644,
+    "K": 0.56096272,
+    "X": 0.14092016,
+    "J": 0.09752181,
+    "Q": 0.08367550,
+    "Z": 0.05128469,
+}
+
+
+def score_english(s):
+    """
+    Calculate and return a score for the provided string based on how likely it
+    is to be English. Right now it's only a relative scale with no bounding.
+    """
+
+    # Get frequency of each character
+    count = Counter()
+    for c in s:
+        count[c.upper()] += 1
+
+    # For each letter in the alphabet, see if this string's frequency % is
+    # similar and score on how close it is.
+    score = 0.0
+    for key, value in ENG_FREQ_MAP.iteritems():
+        freq = float(count[key]) / float(len(s)) * 100
+        score += freq * value
+
+    return score
---
+++ s1chal3.py
@@ -0,0 +1,15 @@
+import crypto
+
+input = crypto.hex_to_str("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+
+
+bestscore = 0
+for c in range(256):
+    s = crypto.xorstring(chr(c) * len(input), input)
+    score = crypto.score_english(s)
+    if score > bestscore:
+        best = s
+        bestscore = score
+        bestchar = c
+
+print "Key: '{}' ({}): {}".format(chr(bestchar), bestscore, best)
