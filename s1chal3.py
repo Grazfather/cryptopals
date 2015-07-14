e0ef55e9cb9519ef60ac6efc6192a0c1f5c02b1a
Grazfather <Grazfather@gmail.com>
2015-07-13 21:03:56 -0700

    S1chal3: Fix var names

--- s1chal3.py
+++ s1chal3.py
@@ -4,12 +4,12 @@ input = crypto.hex_to_str("1b37373331363f78151b7f2b783431333d78397828372d363c783
 
 
 bestscore = 0
-for c in range(256):
-    s = crypto.xorstring_key(input, chr(c))
+for k in range(256):
+    s = crypto.xorstring_key(input, chr(k))
     score = crypto.score_english(s)
     if score > bestscore:
         best = s
         bestscore = score
-        bestchar = c
+        bestkey = k
 
-print "Key: '{}' ({}): {}".format(chr(bestchar), bestscore, best)
+print "Key: '{}' ({}): {}".format(chr(bestkey), bestscore, best)
