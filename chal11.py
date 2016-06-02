9bb61309319c4bf2a761584c916cd3859badbe30
Grazfather <Grazfather@gmail.com>
2016-06-01 22:17:57 -0700

    Set2: Complete challenge 11: ECB/CBC detection oracle

--- chal11.py
+++ chal11.py
@@ -21,4 +21,26 @@ def random_encrypt(data):
         iv = get_random_aes_key()
         return crypto.aes_encrypt_cbc(data, key, iv)
 
-print random_encrypt(b"test").encode("hex")
+def detect_cipher_mode():
+    """
+    Detect which mode a black box AES encryption routine is using.
+    This can be done by providing a block large enough that, even with 5-10
+    bytes of random padding on either end, there will be two blocks of
+    plaintext that are the same. If the corresponding ciphertexts are the same,
+    then we know the mode if ECB, otherwise it's CBC.
+    """
+    # plaintext is 54 bytes of the same value. This way, no matter the padding
+    # (5-10 bytes), we'll have two blocks of all 'A's.
+    pt = b"A"*11 + b"A"*32 + b"A"*11
+    ct = random_encrypt(pt)
+
+    # Split it into blocks
+    blocks = [ct[i:i+16] for i in range(0, len(ct), 16)]
+
+    # Check for duplicates
+    if len(blocks) != len(set(blocks)):
+        print "ECB!"
+    else:
+        print "CBC!"
+
+detect_cipher_mode()
